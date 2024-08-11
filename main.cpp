b47f4f1d2e4d18a6d4f7e4d70eb9ecf14ccd7554
Arda Denizer <ardadenizer95@gmail.com>
2024-08-11 20:27:00 +0100

    Add background image

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
-#include "mainwindow.h"
-
+#include <QMainWindow>
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QMainWindow window;
+    window.setStyleSheet("QMainWindow { background-image: url('../../Assets/background.png'); background-position: center; background-repeat: no-repeat; }");
+    window.resize(800, 600); // Set window size
+    window.show();
+    return app.exec();
 }
